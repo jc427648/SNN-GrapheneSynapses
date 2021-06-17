b2234161b484c032787e74f3ecbf526dcd73e60f
Corey Lammie <coreylammie@gmail.com>
2021-06-17 22:21:04 +1000

    Added full training results

--- Train.py
+++ Train.py
@@ -25,22 +25,35 @@ image_threshold = 50  # Threshold to generate greyscale input images
 
 if __name__ == "__main__":
     # torch.set_default_tensor_type('torch.cuda.FloatTensor')
-    print('Loading MNIST training samples...')
+    print("Loading MNIST training samples...")
     # Define the network architecture
     network = Network(n_output_neurons, n_samples_memory, dt=dt)
     # Train the network
     training_data, training_labels = getMNIST(
-        lower_freq=lower_freq, upper_freq=upper_freq, threshold=image_threshold, dt=dt)
+        lower_freq=lower_freq, upper_freq=upper_freq, threshold=image_threshold, dt=dt
+    )
     start_time = timeit.default_timer()  # Start timer
     for epoch in range(n_epochs):
         n_samples = training_labels.numel()
         for idx in range(n_samples):
             image, label = training_data[idx], training_labels[idx]
             network.presentImage(image, label, image_duration)
-            if (idx % 1000 == 0):
-                print('Training progress: sample (%d / %d) of epoch (%d / %d) - Elapsed time: %.4f'
-                      % (idx, n_samples, epoch + 1, n_epochs, timeit.default_timer() - start_time))
-                plotWeights(ReshapeWeights(network.synapse.w, n_output_neurons)[0],
-                            network.synapse.wmax, network.synapse.wmin, title='idx_%d' % idx)
-                if idx == 1000:
-                    break
+            if idx % 1000 == 0:
+                print(
+                    "Training progress: sample (%d / %d) of epoch (%d / %d) - Elapsed time: %.4f"
+                    % (
+                        idx,
+                        n_samples,
+                        epoch + 1,
+                        n_epochs,
+                        timeit.default_timer() - start_time,
+                    )
+                )
+                plotWeights(
+                    ReshapeWeights(network.synapse.w, n_output_neurons)[0],
+                    network.synapse.wmax,
+                    network.synapse.wmin,
+                    title="idx_%d" % idx,
+                )
+                # if idx == 1000:
+                #     break
