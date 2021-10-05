7632ab631583ba1631ae42a2a8c6bb0f7864b4aa
coreylammie <coreylammie@gmail.com>
2021-10-05 15:44:50 +1000

    Modified PBS Queue for Evaluation Scripts

--- SNN-GrapheneSynapses_300.sh
+++ SNN-GrapheneSynapses_300.sh
@@ -4,8 +4,8 @@
 #PBS -P JCU-SNN
 #PBS -o /scratch/user/coreylammie/SNN-GrapheneSynapses/logs/300_out.txt
 #PBS -e /scratch/user/coreylammie/SNN-GrapheneSynapses/logs/300_error.txt
-#PBS -q Single
-#PBS -l walltime=167:00:00
+#PBS -q Short
+#PBS -l walltime=24:00:00
 #PBS -l select=1:mem=10gb:ncpus=8:mpiprocs=8
 #PBS -m abe -M corey.lammie@jcu.edu.au
 
