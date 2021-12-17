e41289c1b962e1615b7400783d0fab4abfce360b
Corey Lammie <coreylammie@gmail.com>
2021-12-17 16:13:21 +1000

    Added VTEAM_fit.m

--- TO SORT/Evaluate_10.py
+++
@@ -1,8 +0,0 @@
-from Evaluate import evaluate
-
-if __name__ == "__main__":
-    n_output_neurons = 10
-    tau = 0.14986746754094
-    gamma = 0.00630155728720963
-    test_set_accuracy = evaluate(n_output_neurons, tau, gamma)
-    print(test_set_accuracy)
--- TO SORT/Evaluate_500.py
+++
@@ -1,8 +0,0 @@
-from Evaluate import evaluate
-
-if __name__ == "__main__":
-    n_output_neurons = 500
-    tau = 0.074393419260783
-    gamma = 0.0076390882373023
-    test_set_accuracy = evaluate(n_output_neurons, tau, gamma)
-    print(test_set_accuracy)
