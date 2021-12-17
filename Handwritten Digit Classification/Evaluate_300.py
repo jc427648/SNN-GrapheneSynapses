15beccbeb66edefc1db685c896048e56a2dfea04
Corey Lammie <coreylammie@gmail.com>
2021-12-17 16:33:46 +1000

    Updated Evaluate_300.py

--- Handwritten Digit Classification/Evaluate_300.py
+++ Handwritten Digit Classification/Evaluate_300.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 import torch
 import os
-from Mailgun import send_email
 from set_all_seeds import set_all_seeds
 import joblib
 from joblib import Parallel, delayed
@@ -11,15 +10,14 @@ from threadpoolctl import threadpool_limits
 
 
 if __name__ == "__main__":
-    n_output_neurons = 500
-    tau = 0.0845993883511427
-    gamma = 0.00993280807733143
+    n_output_neurons = 300
+    n_epochs = 3
+    tau = 0.074826
+    gamma = 0.006441
     set_all_seeds(0)
-
     def f(i):
         with threadpool_limits(limits=1, user_api='blas'):
-            test_set_accuracy = evaluate(n_output_neurons, tau, gamma, n_epochs=3)
-            print(test_set_accuracy)
+            test_set_accuracy = evaluate(n_output_neurons, tau, gamma, n_epochs=n_epochs)
             return test_set_accuracy
 
     test_set_accuraies = Parallel(n_jobs=10)(delayed(f)(i) for i in range(10))
@@ -29,4 +27,3 @@ if __name__ == "__main__":
     std = test_set_accuraies.std()
     print(mean)
     print(std)
-    send_email('Awoogna', 'Evaluate_500 (5) Executed', 'mean: %f, std: %f' % (mean, std))
